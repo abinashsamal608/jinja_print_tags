5075e968bb3c690c51e1ca6389080b7d60dd7122
abinashsamal608 <abinashsamal608@gmail.com>
2023-07-27 12:07:18 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def data_render(request):
-    d = {'Name':'ABINASH', 'Course':'Python FullStack', 'B_Code':'PYD-M2'}
+    d = {'Name':'ABINASH', 'Course':'Python FullStack', 'B_Code':'PYD-M2','Age':23}
 
     return render(request, 'data_render.html', context =d)
